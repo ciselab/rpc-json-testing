ed3eabb1219b748b3c8a7bb49ccf329eec538821
Lisette <lisette_stella@hotmail.com>
2021-04-10 13:34:02 +0200

    Oops, fix server url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -41,8 +41,8 @@ public class Main {
             // TODO (later): find other APIs to connect to
 
             // The url for the Ripple JSON-RPC API ledger (testnet)
-            String url_ripple = "https://s.altnet.rippletest.net:51234";
-//            String url_ripple = "http://127.0.0.1:5005";
+//            String url_ripple = "https://s.altnet.rippletest.net:51234";
+            String url_ripple = "http://127.0.0.1:5005";
             URL url = new URL(url_ripple);
             Client client = new Client(url);
 
