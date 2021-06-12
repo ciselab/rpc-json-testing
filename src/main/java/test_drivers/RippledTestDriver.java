d016734c9cc76b58fe02c59f8d49ca6f5c8f1469
Lisette <lisette_stella@hotmail.com>
2021-06-12 21:06:34 +0200

    Try simple way

--- src/main/java/test_drivers/RippledTestDriver.java
+++ src/main/java/test_drivers/RippledTestDriver.java
@@ -16,7 +16,22 @@ public class RippledTestDriver extends TestDriver {
 
     public void prepareTest() throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("/bin/bash", "startRippled.sh");
+//        processBuilder.command("/bin/bash", "startRippled.sh");
+        processBuilder.command("kill", "$(lsof -t -i:5005)");
+        processBuilder.command("sleep", "5");
+        processBuilder.command("cd", "rippled-1.6.0/build/cmake/coverage");
+        processBuilder.command("./rippled", "-a", "-v", "--debug", "&", "disown");
+        processBuilder.command("sleep", "5");
+
+//        # kill current rippled server at port 5005
+//        kill $(lsof -t -i:5005)
+//        sleep 120
+//
+//# start rippled server again
+//        cd rippled-1.6.0/build/cmake/coverage
+//            ./rippled -a -v --debug & disown
+//        sleep 120
+
         Process process = processBuilder.start();
 
         printResults(process);
