701a56b70ea9a108bf155154262860a54ba8abd6
Lisette <lisette_stella@hotmail.com>
2021-06-12 20:59:40 +0200

    Try with more time

--- startRippled.sh
+++ startRippled.sh
@@ -1,8 +1,8 @@
 # kill current rippled server at port 5005
 kill $(lsof -t -i:5005)
-sleep 5
+sleep 120
 
 # start rippled server again
 cd rippled-1.6.0/build/cmake/coverage
 ./rippled -a -v --debug & disown
-sleep 5
+sleep 120
