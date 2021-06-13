bb0478a5d9ef234d2b61088d621fb718d026ecba
Lisette <lisette_stella@hotmail.com>
2021-06-13 17:29:36 +0200

    Create accounts through rippled genesis account in testdriver

--- src/main/java/test_drivers/RippledTestDriver.java
+++ src/main/java/test_drivers/RippledTestDriver.java
@@ -26,9 +26,60 @@ public class RippledTestDriver extends TestDriver {
         System.out.println("Process ended with rc =" + rc);
     }
 
-    public ResponseObject runTest(Individual individual) throws Exception {
-        prepareTest();
-        ResponseObject responseObject = getClient().createRequest(individual.getHTTPMethod(), individual.toRequest());
+    private ResponseObject retrieveAccounts() throws IOException {
+        JSONObject request = new JSONObject();
+        request.put("method", "wallet_propose");
+        JSONArray params = new JSONArray();
+        JSONObject paramObj = new JSONObject();
+        paramObj.put("key_type", "secp256k1");
+        params.put(0, paramObj);
+        request.put("params", params);
+        return getClient().createRequest("POST", request);
+    }
+
+    private ResponseObject createAccounts(JSONObject accounts) throws IOException {
+        JSONObject request = new JSONObject();
+        request.put("method", "submit");
+        JSONArray params = new JSONArray();
+        JSONObject paramObj = new JSONObject();
+        paramObj.put("key_type", "secp256k1");
+        paramObj.put("secret", "snoPBrXtMeMyMHUVTgbuqAfg1SUTb"); // genesis secret
+
+        JSONObject txJson = new JSONObject();
+        txJson.put("TransactionType", "Payment");
+        JSONObject amount = new JSONObject();
+        amount.put("currency", "XRP");
+        amount.put("issuer", "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"); // genesis account
+        accounts.put("value", "2000");
+        txJson.put("Account", "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"); // genesis account
+        txJson.put("Destination", accounts.getJSONObject("results").getString("account_id"));
+
+        paramObj.put("tx_json", txJson);
+        params.put(0, paramObj);
+        request.put("params", params);
+
+        return getClient().createRequest("POST", request);
+    }
+
+    private JSONObject replaceAccountStrings(JSONObject request, JSONObject accounts) {
+        return new JSONObject(request.toString().replace("__ACCOUNT__", accounts.getJSONObject("results").getString("account_id")));
+    }
+
+    public ResponseObject runTest(String method, JSONObject request) throws Exception {
+        startServer();
+        System.out.println("PROPOSE WALLETS");
+        ResponseObject accounts = retrieveAccounts();
+        System.out.println(accounts.getResponseCode());
+        System.out.println(accounts.getResponseObject());
+        System.out.println("SENDING FROM GENESIS ACCOUNT");
+        ResponseObject createAccounts = createAccounts(accounts.getResponseObject());
+        System.out.println(createAccounts.getResponseCode());
+        System.out.println(createAccounts.getResponseObject());
+        System.out.println("REPLACE ACCOUNTS IN REQUEST");
+        request = replaceAccountStrings(request, accounts.getResponseObject());
+
+        System.out.println("MAKE REQUEST");
+        ResponseObject responseObject = getClient().createRequest(method, request);
 
         return responseObject;
     }
