fa55dc558ba8cc8b7b3efee11bb85cd8868b0f6f
Lisette <lisette_stella@hotmail.com>
2021-07-20 11:30:53 +0200

    Add ganache dockerfile/script and update rippled dockerfile/script

--- docker/ganache/script.sh
+++ docker/ganache/script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# reset coverage
-cd rippled-1.6.0
-find . -type f -name "*.gcda" -delete
-cd ..
+# (!!!) reset coverage is not being done
 
 # run the tool
 cd blockchain-testing
@@ -11,39 +8,39 @@ cd blockchain-testing
 java -jar target/blockchain-testing-1.0-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3
 cd ..
 
-# kill current rippled server at port 5005
-id=$(lsof -t -i:5005 -sTCP:LISTEN)
+# kill the process
+id=$(lsof -t -i:8545 -sTCP:LISTEN)
 echo "killing: $id"
 kill "$id"
-while [ "$(lsof -t -i:5005 -sTCP:LISTEN)" != "" ]; do
- echo "waiting at kill...  -$(lsof -t -i:5005 -sTCP:LISTEN)-"
+while [ "$(lsof -t -i:8545 -sTCP:LISTEN)" != "" ]; do
+ echo "waiting at kill...  -$(lsof -t -i:8545 -sTCP:LISTEN)-"
  sleep 0.1
 done
 
 # compute test coverage achieved by running the tool
-cd rippled-1.6.0
-gcovr -b -r -s ./ -o final_coverage_total.txt
+cd ganache-cli
+# reset coverage?
+nyc report --reporter=lcov --reporter=text
 echo "This run was based on server: " $3", fitness function:" $1 ", and time:" $2 "minutes." >> final_coverage_total.txt cat final_coverage_total.txt
+cd ..
 
 # reset coverage and run the generated tests
-find . -type f -name "*.gcda" -delete 
-cd ..
+# (!!!) reset coverage is not being done
 cd blockchain-testing
 mvn clean test -Dtest=generated.ind*
-cd ..
 
-# kill current rippled server at port 5005
-id=$(lsof -t -i:5005 -sTCP:LISTEN)
+# kill the process
+id=$(lsof -t -i:8545 -sTCP:LISTEN) # Without the LISTEN part the Java process will be killed as well
 echo "killing: $id"
 kill "$id"
-while [ "$(lsof -t -i:5005 -sTCP:LISTEN)" != "" ]; do
- echo "waiting at kill...  -$(lsof -t -i:5005 -sTCP:LISTEN)-"
+while [ "$(lsof -t -i:8545 -sTCP:LISTEN)" != "" ]; do
+ echo "waiting at kill...  -$(lsof -t -i:8545 -sTCP:LISTEN)-"
  sleep 0.1
 done
 
 # compute test coverage achieved by running the generated tests
-cd rippled-1.6.0
-gcovr -b -r -s ./ -o final_coverage_archive.txt
+cd ganache-cli
+nyc report --reporter=lcov --reporter=text
 echo "These archive tests were based on server: " $3", fitness function:" $1 ", and time:" $2 "minutes." >> final_coverage_archive.txt cat final_coverage_archive.txt
 
 exit
