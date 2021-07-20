10392ce179093d81bc83ce363ed8819ed2c9733c
Lisette <lisette_stella@hotmail.com>
2021-07-20 13:13:39 +0200

    Add statisticskeeper (for now records coverage over time)

---
+++ src/main/java/info/StatisticsKeeper.java
@@ -0,0 +1,44 @@
+package info;
+
+import util.Triple;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatisticsKeeper {
+    private long startTime;
+
+    private List<Triple<Long, Double, Double>> coverageOverTime;
+
+    public StatisticsKeeper() throws IOException {
+        this.startTime = System.nanoTime();
+        this.coverageOverTime = new ArrayList<>();
+    }
+
+    /**
+     * Record the coverage of a certain time.
+     * @param time
+     * @param branchCoverage
+     * @param lineCoverage
+     * @throws IOException
+     */
+    public void recordCoverage(long time, double branchCoverage, double lineCoverage) throws IOException {
+        this.coverageOverTime.add(new Triple<>(time, branchCoverage, lineCoverage));
+
+        // Append coverage to file immediately
+        FileWriter writer = new FileWriter("coverage_over_time.txt", true);
+        for (int i = 0; i < coverageOverTime.size(); i++) {
+            Long t = coverageOverTime.get(i).getKey();
+            double bc = coverageOverTime.get(i).getValue();
+            double lc = coverageOverTime.get(i).getValue2();
+            writer.write("Time: " + t + ", branch coverage: " + bc +  ", line coverage: " + lc + System.lineSeparator());
+        }
+        writer.close();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+}
