9ab020cf94087dc3379c14966c4ade564d0d0874
Lisette <lisette_stella@hotmail.com>
2022-03-15 13:55:46 +0100

    Remove killing of the server again

--- scripts/rippled/startRippled.sh
+++ scripts/rippled/startRippled.sh
@@ -2,10 +2,8 @@
 
 
 # kill current rippled server at port 5005
-id=$(lsof -t -i:5005 -sTCP:LISTEN) # without the LISTEN part the Java process will be killed as well
+# without the LISTEN part the Java process will be killed as well
 
-echo "killing: $id"
-kill "$id"
 count=0
 
 while [ "$(lsof -t -i:5005 -sTCP:LISTEN)" != "" ]; do
@@ -27,19 +25,5 @@ echo "starting server at port 5005"
 # start rippled server again
 cd /rippled-1.6.0/build/cmake/coverage
 ./rippled -a --start -v --debug &
-
-count=0
-
-while [ "$(lsof -t -i:5005 -sTCP:LISTEN)" != "" ]; do
- echo "waiting at kill...  -$(lsof -t -i:5005 -sTCP:LISTEN)-"
- if [[ $count == 1000 ]]; then
-   id=$(lsof -t -i:5005 -sTCP:LISTEN)
-   echo "killing: $id"
-   kill "$id"
-   count=0
- fi
- count=$((count+1))
- sleep 0.1
-done
 
 exit 0
