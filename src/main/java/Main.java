f1d79a704b2302359f29cf08bb7ae44cf07ab4b0
Lisette <lisette_stella@hotmail.com>
2022-04-21 17:58:31 +0200

    Use sample of openrpc specification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -98,7 +98,7 @@ public class Main {
             filepath = directory + System.getProperty("file.separator") + "ripple-openrpc.json";
             url_server = "http://127.0.0.1:5005";
         } else {
-            filepath = directory + System.getProperty("file.separator") + "ripple-openrpc-small.json";
+            filepath = directory + System.getProperty("file.separator") + "ripple-openrpc.json";
             url_server = "https://s.altnet.rippletest.net:51234"; // The url for the Ripple JSON-RPC API ledger (testnet)
         }
 
